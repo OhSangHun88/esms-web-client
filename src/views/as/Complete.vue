b956b6d359a4660d278892a816505f980cfd849f
OhSangHun88 <97290954+OhSangHun88@users.noreply.github.com>
2022-04-26 18:28:36 +0900

    AS수정

--- src/views/as/Complete.vue
+++ src/views/as/Complete.vue
@@ -96,21 +96,20 @@
                     <table>
                         <colgroup>
                             <col style="width:4%;">
-                              <col style="width:6%;">
-                              <col style="width:6%;">
-                              <col style="width:6%;">
-                              <col style="width:6%;">
-                              <col style="width:6%;">
-                              <col style="width:6%;">
-                              <col style="width:6%;">
-                              <col style="width:6%;">
-                              <col style="width:auto;">
-                              <col style="width:auto;">
-                              <col style="width:6%;">
-                              <col style="width:6%;">
-                              <col style="width:6%;">
-                              <col style="width:6%;">
-                            
+                            <col style="width:6%;">
+                            <col style="width:6%;">
+                            <col style="width:6%;">
+                            <col style="width:6%;">
+                            <col style="width:6%;">
+                            <col style="width:6%;">
+                            <col style="width:6%;">
+                            <col style="width:6%;">
+                            <col style="width:auto;">
+                            <col style="width:auto;">
+                            <col style="width:6%;">
+                            <col style="width:6%;">
+                            <col style="width:6%;">
+                            <col style="width:6%;">
                         </colgroup>
                         <thead>
                             <tr>
@@ -128,7 +127,7 @@
                                 <th scope="col">A/S타입</th>
                                 <th scope="col">A/S상태</th>
                                 <th scope="col">요청사항설명</th>
-                                <th scope="col">요청일시</th>
+                                <th scope="col">완료일시</th>
                             </tr>
                         </thead>
                     </table>
@@ -168,13 +167,12 @@
                                   <td>{{changeAsStateCode(item.asStateCd)}}</td>
                                   <td>{{item.requestDesc}}</td>
                                   <td>{{item.updDtime}}</td>
-                              </tr>
+                              </tr>                                
                             </tbody>
-                  
                         </table>
                     </div>
                 </div>
-                <div class="pagination mt0">
+        <div class="pagination mt0">
 					<a href="#" class="front">첫 페이지</a>
 					<a href="#" class="prev">이전 페이지</a>
 					<a href="#" class="on">1</a>
@@ -217,7 +215,7 @@ export default {
         orgSido:'', orgSgg:'', orgCode:'',
         cBirthday:'', cAddr: '', NCount: 0,
         selectedSidoItems:'', selectedSggItems:'', selectedOrgItems:'', selectedRecipientNm: '',
-        errorpopup1: false, errorpopup2: false,asCompleteData: null,
+        errorpopup1: false, errorpopup2: false, asRequestData: null,
       }
     },
     created() {
@@ -228,7 +226,7 @@ export default {
     this.s_date=moment().subtract(6, 'days').format('YYYY-MM-DD');
     this.e_date=moment().format('YYYY-MM-DD');
     this.cBirthday=moment().format('YYYY-MM-DD');
-    this.getAsCompleteList();
+    this.getAsRequestList();
     },
     
     methods:{
@@ -422,20 +420,22 @@ export default {
         }
         return result
     },
-    async getAsCompleteList(){
-      const url  = this.$store.state.serverApi + `/admin/as/list.do?asStateCd=ste003&pageIndex=1&recordCountPerPage=100`
-      await axios.get(url, {headers: {"Authorization": "Bearer " + sessionStorage.getItem("token")}})
-      .then(res => {
-          this.asCompleteData = res.data.data
-          console.log("as 요청")
-          console.log(this.asCompleteData)
-      })
-      .catch(error => {
-          console.log("fail to load")
-          this.errorMessage = error.message;
-          console.error("There was an error!", error);
-      });
-    },
+    async getAsRequestList(){
+        const url  = this.$store.state.serverApi + `/admin/as/list.do?asStateCd=STE007&pageIndex=0&recordCountPerPage=100`
+            await axios.get(url, {headers: {"Authorization": "Bearer " + sessionStorage.getItem("token")}})
+            .then(res => {
+                this.asRequestData = res.data.data
+                console.log("as 요청")
+                console.log(this.asRequestData)
+            })
+            .catch(error => {
+                console.log("fail to load")
+                this.errorMessage = error.message;
+                console.error("There was an error!", error);
+            });
+      },
+
+
   },
 }
 </script>
